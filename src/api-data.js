b389e0c49b6d277b40be274291281fd4206fc2af
applefrittr <jacob.lee.jordan@gmail.com>
2022-10-26 18:31:14 -0400

    UI changes and styling added.  API fetch and display functions complete

--- src/api-data.js
+++ src/api-data.js
@@ -1,5 +1,6 @@
 import * as elements from "./index.js";
 
+// async function which pulls a landscape picture with the query target tag from unsplash.com
 export async function LocationPic(query) {
   const picture = await fetch(
     `https://api.unsplash.com/search/photos?query=${
@@ -8,38 +9,59 @@ export async function LocationPic(query) {
     { mode: "cors" }
   );
   const picData = await picture.json();
+
+  // Since our fetch request will return data on hundreds of photos with the query tag, pick one at random from the first 5
+  const num = Math.floor(Math.random() * 5)
+  const randomImg = picData.results[num]
+  console.log(randomImg)
   // conditional to execute picture fade when a new query is searched.  The layered background elements use css-transition: opacity and class toggling
   // to give the fade effect between images
   if (elements.background1.classList.contains("active-bg")) {
-    elements.background2.style.backgroundImage = `url(${picData.results[0].urls.regular})`;
+    elements.background2.style.backgroundImage = `url(${randomImg.urls.regular}`;
     elements.background1.classList.toggle("active-bg");
     elements.background2.classList.toggle("active-bg");
   } else {
-    elements.background1.style.backgroundImage = `url(${picData.results[0].urls.regular})`;
+    elements.background1.style.backgroundImage = `url(${randomImg.urls.regular}`;
     elements.background2.classList.toggle("active-bg");
     elements.background1.classList.toggle("active-bg");
   }
+
+  return {
+    photographer: `${randomImg.user.first_name} ${randomImg.user.last_name}`,
+    link: randomImg.user.links.html
+  }
 }
 
+// async function that pulls weather and location info of the target query from openweather.org
 export async function LocationWeather(query) {
+  const key = "89241ba92f96e488e73448b0513d1f8b";
+  
+  // geo locate API call to openweather.org, to pull location info, specifically state and country
+  const locale = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${
+      query ? query : "new york"
+    }&limit=1&appid=${key}`,
+    { mode: "cors" }
+  );
+
+  const localeData = await locale.json();
+
+  // weather API call to openweather.org, pull all relevent weather info as well as current time at query location
   const weather = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${
       query ? query : "new york"
     }&units=imperial&appid=89241ba92f96e488e73448b0513d1f8b`,
     { mode: "cors" }
   );
-  const locale = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${
-      query ? query : "new york"
-    }&limit=1&appid=89241ba92f96e488e73448b0513d1f8b`,
-    { mode: "cors" }
-  );
+  
   const weatherData = await weather.json();
-  const localeData = await locale.json();
 
-  console.log(weatherData);
-  console.log(localeData);
+  // console.log(localeData);
+  // console.log(weatherData);
+  
+  const time = new Date((weatherData.dt + weatherData.timezone) * 1000).toUTCString().slice(0, 22);
 
+  // return an object which contains all info weather and location info to be displayed to the user
   return {
     icon: weatherData.weather[0].icon,
     weather: weatherData.weather[0].description,
@@ -49,6 +71,9 @@ export async function LocationWeather(query) {
     wind: weatherData.wind.speed,
     humidity: weatherData.main.humidity,
     state: localeData[0].state ? localeData[0].state : null,
-    country: localeData[0].country
+    country: localeData[0].country,
+    min: weatherData.main.temp_min,
+    max: weatherData.main.temp_max,
+    time
   };
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import * as API from "./api-data.js";
 
 const searchValue = document.querySelector("#search");
 const btn = document.querySelector("button");
+const weatherInfo = document.querySelector("#weather-container");
+const banner = document.querySelector("#banner");
 
 export const background1 = document.querySelector(".background1");
 export const background2 = document.querySelector(".background2");
@@ -11,26 +13,46 @@ const name = document.querySelector("#name");
 const temp = document.querySelector("#temp");
 const feel = document.querySelector("#feel");
 const icon = document.querySelector("#icon");
+const humidity = document.querySelector("#humidity");
+const wind = document.querySelector("#wind");
+const time = document.querySelector("#time");
+const range = document.querySelector("#range");
+const photographer = document.querySelector("#photographer");
 const iconDescription = document.querySelector("#icon-descrip");
 
 background1.style.backgroundImage =
   "url(https://images.unsplash.com/photo-1475522003475-eb5f96f1f930?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNzQ5NDB8MHwxfHNlYXJjaHw4fHxjYWxpZm9ybmlhfGVufDB8fHx8MTY2NjYzNTI3MA&ixlib=rb-4.0.3&q=80&w=1080)";
+background1.style.backgroundPosition = "50% 40%";
 
 btn.addEventListener("click", Location);
 
 // our async function executed when the user searches a new city.  The Unsplash API is used to retrieve a tagged picture of the searched city
 // and the Open Weather API is used to retrieve weather data
 async function Location() {
-  let keyword = searchValue.value;
+  banner.style.display = "none";
+  weatherInfo.style.opacity = "1";
+  let keyword = searchValue.value; // User query value which will be used in our fetch requests to Unsplash and Openweather
 
-  API.LocationPic(keyword);  // function to pull pic of locale from unsplash.com
+  let weatherObj = await API.LocationWeather(keyword); // fucntion to pull weather data of locale from Openweather.org
 
-  let weatherObj = await API.LocationWeather(keyword); // fucntion to pull weather data of locale from openweather.org
+  let photoObj = await API.LocationPic(keyword); // function to pull pic of locale from Unsplash.com along with photographer info (for crediting purposes)
+  console.log(photoObj);
+  // console.log(weatherObj);
 
-  console.log(weatherObj);
-
-  name.textContent = `${weatherObj.name}${weatherObj.state ?', ' + weatherObj.state : ''}${weatherObj.country == 'US' ? '' :', ' + weatherObj.country}`;
-  temp.textContent = `${Math.floor(weatherObj.temp)} \u00B0F`
-  feel.textContent = `Feels like: ${Math.floor(weatherObj.feel)} \u00B0F`
+  name.textContent = `${weatherObj.name}${
+    weatherObj.state ? ", " + weatherObj.state : ""
+  }${weatherObj.country == "US" ? "" : ", " + weatherObj.country}`;
+  temp.textContent = `${Math.round(weatherObj.temp)} \u00B0F`;
+  feel.textContent = `Feels like ${Math.round(weatherObj.feel)} \u00B0F with ${
+    weatherObj.weather
+  }`;
   icon.src = `http://openweathermap.org/img/wn/${weatherObj.icon}@2x.png`;
+  humidity.textContent = `Humidity: ${weatherObj.humidity}%`;
+  wind.textContent = `Wind Speed: ${Math.round(weatherObj.wind)} mph`;
+  range.textContent = `A low of ${Math.round(
+    weatherObj.min
+  )} \u00B0F to a high of ${Math.round(weatherObj.max)} \u00B0F`;
+  time.textContent = weatherObj.time;
+  photographer.href = photoObj.link
+  photographer.textContent = photoObj.photographer
 }
